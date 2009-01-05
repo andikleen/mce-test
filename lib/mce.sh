8103f33b1548516bd03fcedc5d28fedaf25121d5
Huang Ying <ying.huang@intel.com>
2009-01-05 13:25:35 +0800

    Fix pseudo MCE filtering
    Pseudo MCE need to be filtered out before comparing. They are
    identified by "STATUS 0x0" before, now it is possible to have "STATUS
    0x80000000000" (MISCV set).
    
    Signed-off-by: Huang Ying <ying.huang@intel.com>

--- lib/mce.sh
+++ lib/mce.sh
@@ -43,7 +43,8 @@ mce_reformat_for_cmp()
     fi
 
     cat $tmpf | tr '\n' '#' | sed '1,$s/##/\n/g' | \
-	grep -v '#STATUS 0x0#' | sort > "$outf"
+	grep-v '#STATUS 0x0#' | \
+	grep -v '#STATUS 0x800000000000000#' | sort > "$outf"
 }
 
 mce_cmp()
