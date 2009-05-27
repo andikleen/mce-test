d2d9dea689a489b391bb62db35907584149b8adc
Wu Fengguang <fengguang.wu@intel.com>
2009-05-27 22:25:08 +0800

    tinjpage: make file clean case work unconditionally
    Truncate file and then create one page and sync it,
    so that the test case works reliably when the file
    is newly created or is already there and dirty.
    
    Signed-off-by: Wu Fengguang <fengguang.wu@intel.com>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tsrc/tinjpage.c
+++ tsrc/tinjpage.c
@@ -222,9 +222,11 @@ static void file_clean(void)
 	char *page;
 	char fn[30];
 	snprintf(fn, 30, TMPDIR "test%d", tmpcount++);
-	int fd = open(fn, O_RDWR|O_CREAT);
+	int fd = open(fn, O_RDWR|O_TRUNC|O_CREAT);
 	if (fd < 0)
 		err("open temp file");
+	write(fd, fn, 4);
+	fsync(fd);
 	page = checked_mmap(NULL, PS, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_POPULATE, fd, 0);
 	close(fd);
 	testmem("file clean", page, MREAD_OK);
