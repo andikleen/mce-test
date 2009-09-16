ff41dfa081545e1ebf3d8ef9b1c9fdddb824aa31
Andi Kleen <ak@linux.intel.com>
2009-09-16 14:38:35 +0200

    Add GPL copyright header to tinjpage
    Also add Fengguang as author
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- tsrc/tinjpage.c
+++ tsrc/tinjpage.c
@@ -2,7 +2,22 @@
  * Test program for Linux poison memory error recovery.
  * This injects poison into various mapping cases and triggers the poison
  * handling.  Requires special injection support in the kernel.
- * Author: Andi Kleen
+ *
+ * tinjpage is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; version
+ * 2.
+ *
+ * tinjpage is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should find a copy of v2 of the GNU General Public License somewhere
+ * on your Linux system; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ *
+ * Authors: Andi Kleen, Fengguang Wu
  */
 #define _GNU_SOURCE 1
 #include <stdio.h>
