d1be787452998a3e7c58590ff07acc2437e90fe3
Jiajia Zheng <jiajia.zheng@intel.com>
2010-02-04 16:31:08 +0800

    Add KVM RAS test suite
    This patch is to add KVM RAS test suite into mce-test, which is a
    collection of test scripts for testing the Linux kernel MCE processing
    features in KVM guest system.
    
    Signed-off-by: Jiajia Zheng <jiajia.zheng@intel.com>
    Signed-off-by: Huang Ying <ying.huang@intel.com>

--- tools/simple_process/simple_process.c
+++ tools/simple_process/simple_process.c
@@ -6,11 +6,10 @@
 int main(void)
 {
     void *p = malloc(128);
-    memset(p, 0, 128);
     printf("allocating 128 bytes of memory\n"); 
     while (1)
     {
-      sleep(1);
+	memset(p, 0, 128);
     }
     return 0;
 }
